d587772bea80f788033866b78fda510434cab280
Michael Josephson <mikej@users.noreply.github.com>
2015-08-31 10:59:49 +0100

    add skeleton code to app

--- sinatra_todo.rb
+++ sinatra_todo.rb
@@ -1,2 +1,15 @@
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/todo_list.db")
+
+# models will go here
+
+DataMapper.finalize.auto_upgrade!
+
 class SinatraTodoApp < Sinatra::Base
+  set :erb, escape_html: true
+
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  # routes will go here
 end
