092c8a3eaa5174709b9b9c68b7d876462ab813bb
Michael Josephson <mikej@users.noreply.github.com>
2015-09-20 12:27:46 +0100

    Completed version of the code after workshop

--- todo_app.rb
+++ todo_app.rb
@@ -1,6 +1,12 @@
 DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/todo_list.db")
 
-# models will go here
+class Task
+  include DataMapper::Resource
+  property :id, Serial
+  property :description, Text
+  property :completed, Boolean, default: false
+  property :created_at, DateTime
+end
 
 DataMapper.finalize.auto_upgrade!
 
@@ -11,10 +17,40 @@ class TodoApp < Sinatra::Base
     register Sinatra::Reloader
   end
 
-  # example route, access this with http://localhost:9292/example
-  get '/example' do
-    "The example worked!"
+  # go straight to the list of tasks if opening e.g. localhost:9292/
+  get '/' do
+    redirect '/todos'
+  end
+  
+  # route for displaying the list of todos
+  get '/todos' do
+    @tasks = Task.all(order: :created_at.desc)
+    erb :todos
   end
 
-  # add more routes here
+  # route for adding a new task to the list
+  post '/todos' do
+    unless params[:description].empty? # prevent adding blank entries
+      Task.create(description: params[:description], created_at: Time.now)
+    end
+    redirect '/todos'
+  end
+
+  # route for ticking a task as completed
+  post '/todos/:id/complete' do
+    task = Task.get(params[:id])
+    task.update(completed: true)
+  end
+
+  # route for unticking a task
+  post '/todos/:id/uncomplete' do
+    task = Task.get(params[:id])
+    task.update(completed: false)
+  end
+
+  # route for deleting a task
+  delete '/todos/:id' do
+    task = Task.get(params[:id])
+    task.destroy
+  end
 end
