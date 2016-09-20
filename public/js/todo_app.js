ce532c7fab6b9cd4702672d4b528435bff867d6f
Michael Josephson <mikej@users.noreply.github.com>
2016-09-20 21:35:47 +0100

    Add completed version of todo app

--- public/js/todo_app.js
+++ public/js/todo_app.js
@@ -1,1 +1,28 @@
-// JavaScript for the todo app will go here
+$(document).ready(function() {
+
+    // event handler for clicking checkboxes
+    $('input.done-checkbox').click(function() {
+        var ticked = $(this).is(':checked'); // check if this checkbox is ticked or unticked
+        $li = $(this).parents('li');
+        var taskId = $li.data('id'); // find the id ID of the task for the checkbox that was clicked
+        var action = ticked ? '/complete' : '/uncomplete'; // pick action based on whether checkbox is ticked
+        $.post('/todos/' + taskId + action, null, function() {
+            $li.toggleClass('completed');
+        });
+    });
+
+    // event handler for deleting tasks
+    $('a.delete-link').click(function() {
+        /// find the ID of the task for this delete link
+        $li = $(this).parents('li');
+        var itemId = $li.data('id');
+        $.ajax({
+            method: 'DELETE',
+            url: '/todos/' + itemId
+        }).done(function(data) {
+            // animation for removing the item after it's been deleted
+            $li.slideUp(200);
+        });
+    });
+
+});
